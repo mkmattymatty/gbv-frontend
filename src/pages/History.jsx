92829f73779bb1a779745466240a41e132d145e3
Mathias Mwaro <mattymatty9372@gmail.com>
2025-11-25 17:34:21 +0300

    Create History component to display GBV history

---
+++ src/pages/History.jsx
@@ -0,0 +1,13 @@
+// frontend/src/pages/History.jsx
+import React from 'react';
+import GBVHistory from '../components/GBVHistory';
+
+const History = () => {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <GBVHistory />
+    </div>
+  );
+};
+
+export default History;
